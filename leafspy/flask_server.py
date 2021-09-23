d904914fe56fb4ae23aa4a2c364490242d0fe4bc
Jan Petter Maehlen <jepe@ife.no>
2021-09-23 14:31:57 +0200

    add a comment about missing fields and some very simple refactorings

--- leafspy/flask_server.py
+++ leafspy/flask_server.py
@@ -1,12 +1,14 @@
 import os
 from re import split
-from flask import Flask, flash, request, redirect, url_for, send_from_directory, jsonify
-from werkzeug.utils import secure_filename
-from data_handler import functions
-from SupportedTests import accepted_files, accepted_combinations, accepted_instruments, accepted_tests
 import random
 import string
 import gzip
+
+from flask import Flask, flash, request, redirect, url_for, send_from_directory, jsonify
+from werkzeug.utils import secure_filename
+
+from data_handler import functions
+from SupportedTests import accepted_files, accepted_combinations, accepted_instruments, accepted_tests
 
 
 UPLOAD_FOLDER = './uploads'
@@ -14,9 +16,11 @@ UPLOAD_FOLDER = './uploads'
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+
 def check_gzip(filename):
     splitted = filename.split(".")
     return len(splitted) > 2 and splitted[2] =="gz"
+
 
 def allowed_test(extension, test_type, instrument):
     if extension not in accepted_files: 
@@ -36,12 +40,28 @@ def allowed_test(extension, test_type, instrument):
     else:
         return (False, f"{test_type} test is not supported in {extension} files.","")
 
+
 @app.errorhandler(404)
 def page_not_found(error):
     return 'This page does not exist', 404
 
+
 @app.route('/upload_file', methods=['GET', 'POST'])
 def upload_file():
+
+    # this is the default generated html/page created
+    # TODO: add fields for brand, instrument, test_type
+
+    out = '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+
     if request.method == 'POST':
         # check if the post request has the file part
         test_type = request.form.get("test_type")
@@ -57,7 +77,7 @@ def upload_file():
         if test_type == "XRD":
             test_type = test_type
         else:
-            test_type = test_type= test_type + "-" + test_type_subcategory
+            test_type = "-".join([test_type, test_type_subcategory])
             
             #if not test_subtype:
             #return {'Code':1, 'Message':'Please provide a subtest type'}
@@ -106,19 +126,13 @@ def upload_file():
             else:
                 return {'Code':1, 'Message': message }
 
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
-      <input type=file name=file>
-      <input type=submit value=Upload>
-    </form>
-    '''
+    return out
+
 
 @app.route('/uploads/<name>')
 def download_file(name):
     return send_from_directory(app.config["UPLOAD_FOLDER"], name)
+
 
 if __name__ == "__main__":
     from waitress import serve
