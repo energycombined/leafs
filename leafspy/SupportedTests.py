86292c08f0051d4e0aae73102019680c507ca222
Jan Petter Maehlen <jepe@ife.no>
2021-10-10 10:52:39 +0200

    Clean up code (including black)

--- leafspy/SupportedTests.py
+++ leafspy/SupportedTests.py
@@ -1,26 +1,23 @@
-
-accepted_files = ['MPR','RES','TXT']
-accepted_tests = ['VOLTAMMETRY-CYCLIC VOLTAMMETRY (CV)', 'CHARGE-DISCHARGE-GALVANOSTATIC CYCLING', 'EIS', 'XRD']
-accepted_instruments = ['ARBIN-BT-2000', 'BIOLOGIC-VMP3', 'BIOLOGIC-MPG2','STOE-STADI P']
-used_functions = ['galvani','cellpy','xrd_custom']
-
+accepted_files = ["MPR", "RES", "TXT"]
+accepted_tests = [
+    "VOLTAMMETRY-CYCLIC VOLTAMMETRY (CV)",
+    "CHARGE-DISCHARGE-GALVANOSTATIC CYCLING",
+    "EIS",
+    "XRD",
+]
+accepted_instruments = [
+    "ARBIN-BT-2000",
+    "BIOLOGIC-VMP3",
+    "BIOLOGIC-MPG2",
+    "STOE-STADI P",
+]
+used_functions = ["galvani", "cellpy", "xrd_custom"]
 
 
 # file -> test -> instrument --> used function
 
 accepted_combinations = {
-    0:{
-        0:{
-            1:'galvani',
-            2:'galvani'},
-    },
-    1:{
-        1:{
-            0:'cellpy'},
-    },
-    2:{
-        3:{
-            3:'xrd_custom'}
-    }
+    0: {0: {1: "galvani", 2: "galvani"}, },
+    1: {1: {0: "cellpy"}, },
+    2: {3: {3: "xrd_custom"}},
 }
-
--- leafspy/flask_server.py
+++ leafspy/flask_server.py
@@ -1,7 +1,10 @@
+"""API and server for Leafs"""
+
 import os
 from pathlib import Path
 from re import split
 import random
+import logging
 import string
 import gzip
 
@@ -9,53 +12,92 @@ from flask import Flask, flash, request, redirect, url_for, send_from_directory,
 from werkzeug.utils import secure_filename
 
 from .data_handler import functions
-from .SupportedTests import accepted_files, accepted_combinations, accepted_instruments, accepted_tests
+from .SupportedTests import (
+    accepted_files,
+    accepted_combinations,
+    accepted_instruments,
+    accepted_tests,
+)
 
 
-UPLOAD_FOLDER = './uploads'
+UPLOAD_FOLDER = "./uploads"
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 
 def temporary_storage_path(extension):
-    new_file_name = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(25)) + "." + extension.lower()
-    location = Path(os.path.join(os.path.join(app.config['UPLOAD_FOLDER'], new_file_name))).resolve()
+    """Path to a temporary directory for storing intermediate file(s)."""
+    new_file_name = (
+        "".join(
+            random.choice(string.ascii_uppercase + string.digits) for _ in range(25)
+        )
+        + "."
+        + extension.lower()
+    )
+    location = Path(
+        os.path.join(os.path.join(app.config["UPLOAD_FOLDER"], new_file_name))
+    ).resolve()
     return location
 
 
 def check_gzip(filename):
+    """Check that the file is a gzip file."""
     return Path(filename).suffix == ".gz"
 
 
 def allowed_test(extension, test_type, instrument):
-    if extension not in accepted_files: 
-        return (False, f"{extension} extension not yet supported, currently we support the following files {accepted_files}","")
+    """Check if the file is appropriate type."""
+    if extension not in accepted_files:
+        return (
+            False,
+            f"{extension} extension not yet supported, currently we support the following files {accepted_files}",
+            "",
+        )
     if test_type not in accepted_tests:
-        return (False, f"{test_type} test not yet supported, currently we support the following tests {accepted_tests}","") 
+        return (
+            False,
+            f"{test_type} test not yet supported, currently we support the following tests {accepted_tests}",
+            "",
+        )
     if instrument not in accepted_instruments:
-        return (False, f"{instrument} not yet supported, currently we support the following instruments {accepted_instruments}","")
+        return (
+            False,
+            f"{instrument} not yet supported, currently we support the following instruments {accepted_instruments}",
+            "",
+        )
     file_index = accepted_files.index(extension)
     test_index = accepted_tests.index(test_type)
     instrument_index = accepted_instruments.index(instrument)
     if test_index in accepted_combinations[file_index].keys():
         if instrument_index in accepted_combinations[file_index][test_index]:
-            return (True, "", accepted_combinations[file_index][test_index][instrument_index])
+            return (
+                True,
+                "",
+                accepted_combinations[file_index][test_index][instrument_index],
+            )
         else:
-            return (False, f"{instrument} {test_type} tests are not supported in {extension} files","")
+            return (
+                False,
+                f"{instrument} {test_type} tests are not supported in {extension} files",
+                "",
+            )
     else:
-        return (False, f"{test_type} test is not supported in {extension} files.","")
+        return False, f"{test_type} test is not supported in {extension} files.", ""
 
 
 @app.errorhandler(404)
 def page_not_found(error):
-    return 'This page does not exist', 404
+    """404."""
+    logging.debug(f"404: {error}")
+    return "This page does not exist", 404
 
 
-@app.route('/upload_file', methods=['GET', 'POST'])
+@app.route("/upload_file", methods=["GET", "POST"])
 def upload_file():
+    """Route for posting files."""
     # This method should probably only handle POST requests
-    out = '''
+    out = """
     <!doctype html>
     <title>Upload new File</title>
     <h1>Upload new File</h1>
@@ -63,59 +105,61 @@ def upload_file():
       <input type=file name=file>
       <input type=submit value=Upload>
     </form>
-    '''
+    """
 
-    if request.method == 'POST':
+    if request.method == "POST":
         # check if the post request has the file part
         test_type = request.form.get("test_type")
         test_type_subcategory = request.form.get("test_type_subcategory")
         instrument = request.form.get("instrument")
         brand = request.form.get("instrument_brand")
         if not brand:
-            return {'Code':1, 'Message':'Please provide an instrument brand'}
+            return {"Code": 1, "Message": "Please provide an instrument brand"}
         if not instrument:
-            return {'Code':1, 'Message':'Please provide an instrument'}
+            return {"Code": 1, "Message": "Please provide an instrument"}
         if not test_type:
-            return {'Code':1, 'Message':'Please provide a test type'}
+            return {"Code": 1, "Message": "Please provide a test type"}
         if test_type == "XRD":
             test_type = test_type
         else:
             test_type = "-".join([test_type, test_type_subcategory])
-            
-            #if not test_subtype:
-            #return {'Code':1, 'Message':'Please provide a subtest type'}
-            
+
+            # if not test_subtype:
+            # return {'Code':1, 'Message':'Please provide a subtest type'}
+
         #    return {'Code':1, 'Message':'Please provide a test subtype'}
         instrument = brand + "-" + instrument
-        #if not test_subtype:
+        # if not test_subtype:
         #    test_type = test_type
-        #else: 
+        # else:
         #    test_type= test_type + "-" + test_subtype
-        files = request.files.getlist('files')
+        files = request.files.getlist("files")
 
         if len(files) == 0:
-            return {'Code':1, 'Message': "No file attached"}
+            return {"Code": 1, "Message": "No file attached"}
 
         # If the user does not select a file, the browser submits an
         # empty file without a filename.
         file = files[0]
 
         if not check_gzip(file.filename):
-            return {'Code':1 , 'Message': "Only gz files allowed"}
-        extension = file.filename.rsplit('.', maxsplit=2)[-2].upper()
+            return {"Code": 1, "Message": "Only gz files allowed"}
+        extension = file.filename.rsplit(".", maxsplit=2)[-2].upper()
         filename = secure_filename(file.filename.rsplit(".", maxsplit=2)[0])
 
-        file = gzip.open(file, 'rb')
+        file = gzip.open(file, "rb")
         file = file.read()
 
-        if filename == '':
-            return {'Code':1, 'Message': "File has no name"}
+        if filename == "":
+            return {"Code": 1, "Message": "File has no name"}
         if file:
-            allowed, message, data_converter = allowed_test(extension, test_type.upper(), instrument.upper())
+            allowed, message, data_converter = allowed_test(
+                extension, test_type.upper(), instrument.upper()
+            )
             if allowed:
                 location = temporary_storage_path(extension)
 
-                with open(location, 'wb') as f_out:
+                with open(location, "wb") as f_out:
                     f_out.write(file)
 
                 success, data = functions[data_converter](location)
@@ -129,18 +173,25 @@ def upload_file():
                     return data
 
                 else:
-                    return{'Code':1, 'Message': "Unknown Error while transforming file."}
+                    return {
+                        "Code": 1,
+                        "Message": "Unknown Error while transforming file.",
+                    }
             else:
-                return {'Code':1, 'Message': message }
+                return {"Code": 1, "Message": message}
 
     return out
 
 
-@app.route('/uploads/<name>')
+@app.route("/uploads/<name>")
 def download_file(name):
+    """Route to get file from uploads folder."""
+
+    # This seems not to be in use?
     return send_from_directory(app.config["UPLOAD_FOLDER"], name)
 
 
 if __name__ == "__main__":
     from waitress import serve
+
     serve(app, host="0.0.0.0", port=8080)
